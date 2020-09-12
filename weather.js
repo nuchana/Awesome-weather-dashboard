6b95fa82edae2836855041f74b07f99bb71ca5a0
Nuchana <nuchana.johnson@gmail.com>
2020-09-12 00:18:02 -0400

    add CSS style

---
+++ weather.js
@@ -0,0 +1,15 @@
+var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
+
+// importing JS file
+
+// call local storage if we save anything 
+
+// create on click event for search
+    // grab information from the input from query URL
+    // store information from input into local storage
+    // make query URL
+    // we can make ajax call
+
+// after we get info back from API
+    // update current city information
+    // look at API call (might be another call)
