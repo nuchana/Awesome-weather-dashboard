5febb10503eedcdf7a1bc12c588f600a8722074a
Nuchana <nuchana.johnson@gmail.com>
2020-09-13 15:04:55 -0400

    fix Ajax

--- weather.js
+++ weather.js
@@ -1,33 +1,63 @@
 $(document).ready(function () {
 
-    // create/listen to click event for city search
+    // create/listen to click event for city search and history
     $("#submitCity").on("click", function () {
         return getWeather();
 
     });
 
     $(".history").on("li", function () {
-        return makeRow();
+        return createRow();
 
     });
+
+
+
+    // when history list click, function makeRow is called
+    function createRow() {
+        console.log(createRow)
+        var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
+        $(".history").append("li");
+
+    };
+
+    function getWeather(searchValue) {
+        // make a request to API openweahtermap
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + searchValue + "&units=imperial + "&appid=8dd0ab36dd50a97450eb53bfcb6ca7dd",
+            method: "GET",
+            dataType: "json",
+
+        })
+            // After the data from AJAX comes back, we show the weather
+            .then(function (data) {
+                console.log(data)
+                //create history link for this search
+                if (history.indexOf(searchValue) === -1) {
+                    history.push(searchValue);
+                    window.localStorage.setItem("history", JSON.stringify(history));
+
+                    createRow(searchValue);
+                }
+
+
+
+
+            });
+
+    }
+
 
     // get current histor if any
     var history = JSON.parse(window.localStorage.getItem("history")) || [];
     // console.log(history)
     if (history.length > 0) {
         getWeather(history[0]);
-
     }
 
     for (var i = 0; i < history.length; i++) {
         makeRow(history[i]);
     }
 
+});
 
-
-    function makeRow() {
-        console.log(makeRow)
-        var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
-        $(".history").append("li");
-
-    });
